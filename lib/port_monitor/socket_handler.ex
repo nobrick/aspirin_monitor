e21ba96fe10f9528644e8869df17c9dd4e4e3869
MQ <qucool@gmail.com>
2016-04-05 10:28:12 +0800

    Update socket handler for broadcasting

--- lib/port_monitor/socket_handler.ex
+++ lib/port_monitor/socket_handler.ex
@@ -1,9 +1,17 @@
 defmodule PortMonitor.SocketHandler do
   use GenEvent
+  alias Aspirin.Endpoint.broadcast!
 
   def handle_event({:test_port, result}, _state) do
-    IO.puts("[INFO] #{inspect result}")
-    Phoenix.Channel.broadcast!(Aspirin.UserSocket, "test_port", result)
+    IO.puts("[BROADCAST] #{inspect result}")
+    case result do
+      :ok ->
+        msg = %{body: :success}
+        broadcast!("status:all", "test_port", msg)
+      {:error, reason} ->
+        msg = %{body: :failure, reason: reason}
+        broadcast!("status:all", "test_port", msg)
+    end
     {:ok, []}
   end
 end
